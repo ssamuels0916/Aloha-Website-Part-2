0d1e0c84e0001f38d6e00a6f44ecaab4374fe1db
Sharifa <shar.samuels@gmail.com>
2016-11-24 11:19:04 -0800

    added scroll to top to logo

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
-$( document ).ready(function() {
-   
+$(function() {
 
 
-   $('a').smoothScroll();
-
-  
-  
-	$('.wrapper').on('submit', function(event) {
-   event.preventDefault();
+    // smoothly scroll when you click on the navigation links
+    $('a').smoothScroll();
 
 
-   		if ( $('#inputText').val() == '' ) {
-      		alert('Please enter a correct email address.');
-  		} else {
-      		alert('Thank you for subscribing to our newsletter!');
-   		}
+    $('.scrollTop').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 5000);
+        return false;
+    });
 
-   		$('#inputText').val('');
+
+    $('.wrapper').on('submit', function(event) {
+        event.preventDefault();
+        if ($('#inputText').val() == '') {
+            alert('Please enter a correct email address.');
+        } else {
+            alert('Thank you for subscribing to our newsletter!');
+        }
+
+        $('#inputText').val('');
+    });
+
 });
-
- });  
-
-
-
-
-
